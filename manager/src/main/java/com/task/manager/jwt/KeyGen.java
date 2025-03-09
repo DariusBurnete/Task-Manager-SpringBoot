82f06150eca7dd10248436ecfca7be8e3f79e4d2
E.on <90399175+DariusBurnete@users.noreply.github.com>
2025-03-09 16:12:09 +0200

    Update KeyGen.java

--- manager/src/main/java/com/task/manager/jwt/KeyGen.java
+++ manager/src/main/java/com/task/manager/jwt/KeyGen.java
@@ -10,8 +10,8 @@ import java.util.Base64;
 
 public class KeyGen{
     public static void main(String[] args) {
-        *//*SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
         String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
-        System.out.println(base64Key);*//*
+        System.out.println(base64Key);
     }
 }*/
