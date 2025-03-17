ea5576aa2c7b71472717c438b63eba36aa6cf32c
DariusBurnete <dariusburnete@yahoo.com>
2025-03-17 13:36:06 +0200

    redirection to login from home

--- manager/src/main/java/com/task/manager/config/SecurityConfig.java
+++ manager/src/main/java/com/task/manager/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/login","/deleteOwner", "/css/**").permitAll()
+                        .requestMatchers("/","/register", "/login","/deleteOwner", "/css/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
